f4b83547878c3b65bd51582a233330a638c20a87
AntiDisbalanS <103729014+AntiDisbalanS@users.noreply.github.com>
2022-06-06 17:33:38 +0300

    Update pwm.v

--- pwm.v
+++ pwm.v
@@ -1,70 +1,86 @@
-`timescale 1ns/1ps
 
-	//Company: 		PLANAR JSK
-	//Engineer: 		Mordashou Anatol
-	//Create Date: 		15/04/2022 
-	//Design Name: 		Pulse-Width Modulation
-	//Module Name: 		pwm
-	//Project Name: 	UKDF_MAX, USDFA 
-	//Target Devices: 	MAX3, Cyclone IV
-	//Tool versions: 	Quartus v9.1 & v13.1
-	//Revision: 		v3 
+//Company: PLANAR JSK
+//Engineer: Mordashou Anatol
+//Create Date: 06/06/2022 
+//Design Name: Pulse Width Modulation
+//Module Name: pwm
+//Project Name: USDFA 
+//Target Devices: Cyclone IV
+//Tool versions: Quatrus v20 & ModelSim 
+//Revision: v4
 
+/* fast copy 
+    pwm #(4,4,1) pwm (
+        .pwm(pwm),
+        .sel_width(sel_width),
+        .sel_clk(sel_clk),
 
-module pwm #(													
-	parameter 	CLK_Q 	= 3, 	//Quantity of frequencies for sel					
-			WIDTH_Q	= 8, 	//Quantity of pulse width for sel						
-			CLK_S	= 3'b0,
-			WIDTH_S	= 8'b0)(
-//Remark: final frequency [MAX : MIX) = clk /2 /2^WIDTH_Q : clk /2 /2^CLK_Q /2^WIDTH_Q	|clk/512 :  clk/65_536]
-	input						wr,
-	output						out,
-	input 	[CLK_Q + WIDTH_Q - 1 : 0]		code,			
-	input 						clk, res);							
+        .s_rst(s_rst),
+        .clk(clk),
+        .rst_n(rst_n));
+*/
 
-	
-	reg	[CLK_Q - 1 : 0]				sel_clk;																							//	|[3:0]
-	reg	[WIDTH_Q - 1 : 0]			sel_width; 																							//	|[7:0]
-	reg	[2 ** CLK_Q - 1 : 0]			count_1; //Width 2^CLK_Q
-	reg						fclk; 
-	reg	[WIDTH_Q - 1 : 0]			count_2;																							//	|[7:0]
-	reg						out_;			
+module pwm #(
+    parameter B_WIDTH = 4, //number of pulse width bits  for sel
+              B_CLK   = 4, //number of frequency bits to select
+              PWM_POL = 1)( //polarity  
+    output reg                pwm,
+    input [ B_WIDTH - 1 : 0 ] sel_width,
+    input [ B_CLK - 1   : 0 ] sel_clk,
 
-					
-				
-	assign out = out_ || &{sel_clk, sel_width}; //out or high
-	
-	always @(negedge wr or negedge res) //write reg for VME or USB
-	
-		if(!res) begin sel_clk <= CLK_S; sel_width <= WIDTH_S; end //starting values
-		else begin
-		
-			sel_clk <= code[CLK_Q + WIDTH_Q - 1 : WIDTH_Q];
-			sel_width <= code[WIDTH_Q - 1 : 0];  
-					
-		end
-	
-	always @(posedge clk or negedge res) 
-	
-		if (!res) begin fclk <= 1'b0; count_1 <= 1'b0;  end	
-		else begin
-			
-			count_1 <= count_1 + 1'b1;	//First counter for divide clk	
-			fclk <= count_1[sel_clk];	//Buffer after first divide. 
-				
-		end
-		
-			
-	always @(posedge fclk or negedge res) 
-	
-		if (!res) begin count_2 <= 1'b0; out_ <= 1'b0;  end //res DFF
-		else begin
-						
-			count_2 <= count_2 + 1'b1;	//Second counter for divide fclk
-			out_ <= (count_2 < sel_width);	//Comparator for duty cycle | ex: 01 - max duty cycle, FF - min duty cycle. 															 
-										
-		end	
-	
+    input                     s_rst,
+    input                     clk,
+    input                     rst_n);
+
+    localparam C_CLK   = 2 ** B_CLK;
+
+    reg   [ B_CLK - 1   : 0 ] sel_clk_buf;
+    reg   [ C_CLK - 1   : 0 ] clk_count;
+    reg   [ 2 : 0 ]           clk_en_buf;
+    wire                      clk_en;
+    reg   [ B_WIDTH - 1 : 0 ] width_count;
+    reg   [ B_WIDTH - 1 : 0 ] sel_width_buf;
+
+    always @(posedge clk or negedge rst_n)
+        if (!rst_n) begin
+            clk_count        <= {C_CLK{1'b0}};
+            sel_clk_buf      <= {B_CLK{1'b0}};
+            clk_en_buf       <= 2'b0;
+        end else begin
+
+            if (s_rst) // counter 1 for create clk
+                clk_count <= {C_CLK{1'b0}};
+            else 
+                clk_count <= clk_count + 1'b1;
+            
+            if (~|width_count) //delay cycle 
+                sel_clk_buf <= sel_clk;   
+            
+            clk_en_buf[0] <= clk_count[sel_clk];
+            clk_en_buf[1] <= clk_en_buf[0];
+        end
+
+    assign clk_en = clk_en_buf[0] & ~clk_en_buf[1]; //create strob
+
+    always @(posedge clk or negedge rst_n)
+        if (!rst_n) begin
+            width_count   <= {B_WIDTH{1'b0}};
+            sel_width_buf <= {B_WIDTH{1'b0}};
+			pwm           <= |PWM_POL;
+        end else begin
+            if (clk_en) //counter 2 for width modulation
+                if (s_rst) 
+                    width_count <= {B_WIDTH{1'b0}}; 
+                else 
+                    width_count <= width_count + 1'b1;
+
+			if (~|width_count) //delay cycle 
+				sel_width_buf <= sel_width; 
+
+            if (clk_en) 
+                if (s_rst) 
+                    pwm         <= |PWM_POL;
+                else 
+                    pwm         <= |PWM_POL ^ (width_count < sel_width_buf); //Comparator for duty cycle 01 - min duty cycle, FF - max duty cycle. 
+        end
 endmodule
-
-
